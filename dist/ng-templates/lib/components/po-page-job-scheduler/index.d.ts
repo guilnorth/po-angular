d128ab768f211f99fe197fadb24cf92495d945db
guilherme.noronha <guilherme.noronha@totvs.com.br>
2022-10-24 15:30:04 -0300

    build files

--- dist/ng-templates/lib/components/po-page-job-scheduler/index.d.ts
+++ dist/ng-templates/lib/components/po-page-job-scheduler/index.d.ts
@@ -1,4 +1,4 @@
 export * from './po-page-job-scheduler.component';
 export * from './po-page-job-scheduler.module';
 export * from './interfaces/po-job-scheduler.interface';
-export * from './po-page-job-scheduler-parameters/po-job-scheduler-parameters-template/po-job-scheduler-parameters-template.directive';
+export * from './po-page-job-scheduler-parameters';
